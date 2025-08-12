d7f95da69f6fe437fc392f4cfd91218250b96f9f
ali <ali@DebianOs.myguest.virtualbox.org>
2025-08-12 23:04:36 +0300

    Added a basic skeleton That has the but the Transaction lists are yet to be added

---
+++ Taho-Interface/src/core/views/Dashboard.vue
@@ -0,0 +1,76 @@
+<!-- TODO:
+Build the base skeleton to the Project.
+Add styling to all the buttons from outside style files.
+-->
+
+<script setup>
+import {ref} from 'vue'
+var current_balance = ref('PlaceHolder for Current Balance')
+var wallet_token = ref('Token string')
+</script>
+<template>
+<div><!-- Main div-->
+
+  <div style="display: flex; gap: 10px;">
+    <div id="balance" class="box center-box"><!-- Balance Div -->
+        <div id="Icon&Type" class="box icon-type">
+          <!-- Here should add Components to the Balance div-->
+          <div class="box">Icon</div>
+          <div class="box">Icon type</div>
+        </div>
+        <br>
+        <p :id= "'${current_balance}'"> {{current_balance}} </p>
+    </div>
+    <div id="preform_transaction" class="box center-box" ><!-- Transaction Div-->
+        <div class="box center-box" style="width:80%;">
+          <div>
+            <span :id="'${wallet_token}'">{{wallet_token}} - </span>
+              <button id="copy_wallet_token" >Copy</button>
+          </div>
+          <br>
+            <div style="display:flex;min-height:40px;border-radius:20px;" >
+              <button class="full-button">Begin Transaction</button>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <br>
+
+  <div id="transaction_list" class="box"><!-- List Div -->
+  </div>
+
+
+</div>
+</template>
+
+<!-- Contains the styles for the divs only -->
+<style scoped>
+.box {
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 10px;
+  min-height: 60px;
+  flex: 1;
+}
+.center-box{
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+}
+.full-button{
+  flex:1;
+  border:1px;
+  border-radius:40px;
+  width:100%;
+  height:100%;
+}
+.icon-type{
+  display:flex;
+  gap:15px;
+  width:50%;
+  align-items:center;
+}
+</style>
+
---
+++ Taho-Interface/src/modules/Transaction_List/models/Transaction.ts
@@ -0,0 +1,6 @@
+class Transaction{
+  private hash: string= "";
+  private amount: number = 0;
+  private status: boolean = false;
+  protected DateTime: Date = new Date();
+}
