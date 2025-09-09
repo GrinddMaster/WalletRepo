5b07b99de0e44895a91847bdb5dc51b1baee271a
ali <ali@DebianOs.myguest.virtualbox.org>
2025-09-09 18:00:10 +0300

    Now I layed down the basic skeleton for the app with the exception to feature in the swap window the next stage is refactoring for the css files that will be used in the vue files and styling the application entire css

--- Taho-Interface/src/core/components/transaction_components/receive_transaction.vue
+++ Taho-Interface/src/core/components/transaction_components/receive_transaction.vue
@@ -1,10 +1,38 @@
-<script></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const showTooltip = ref(false)
+const mouseX = ref(0)
+const mouseY = ref(0)
+
+function handleMouseMove(event: MouseEvent) {
+  mouseX.value = event.clientX + 10
+  mouseY.value = event.clientY + 10
+}
+</script>
+
 <template>
   <div class="receive_box">
     <div class="receive_box"></div>
-    <div class="receive_box"></div>
+    <div
+      class="hash-container2"
+      @mouseenter="showTooltip = true"
+      @mouseleave="showTooltip = false"
+      @mousemove="handleMouseMove"
+    >
+      <span>0x5c51b7af2533504eab84f7f23d21918660fbb78d</span>
+
+      <div
+        v-if="showTooltip"
+        class="info-box2"
+        :style="{ top: mouseY + 'px', left: mouseX + 'px' }"
+      >
+        Copy!
+      </div>
+    </div>
   </div>
 </template>
+
 <style>
 .receive_box {
   border: 1px solid black;
@@ -13,6 +41,31 @@
   min-height: 60px;
   display: flex;
   flex-direction: column;
-  flex: 1;
+  flex: none;
+  width: auto;
+}
+
+.hash-container2 {
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  padding-top: 20px;
+  flex: none;
+  width: auto;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.info-box2 {
+  position: fixed;
+  background-color: #333;
+  color: #fff;
+  padding: 6px 10px;
+  border-radius: 6px;
+  white-space: nowrap;
+  font-size: 12px;
+  pointer-events: none;
+  z-index: 9999;
 }
 </style>
